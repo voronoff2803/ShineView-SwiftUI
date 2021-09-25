b08f42df05681eba408cf16791212b8f786eac59
Алексей <voronoff2803@gmail.com>
2021-09-25 11:53:12 +0300

    gif quality

--- Example.swift
+++ Example.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 .background(Color.blue)
                 .cornerRadius(18)
+            
                 .shineEffect(animationTrigger: $animateTrigger)
         }
     }
